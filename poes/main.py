019d61ab06b19a286842011183069c1a6d4b9186
lissette p <listpita@espol.edu.ec>
2025-11-07 13:11:09 -0500

    values Thickness poes 25

--- poes/main.py
+++ poes/main.py
@@ -1,6 +1,8 @@
 #%% Import Pyhton Libraries
 from poes.model.poes import poes
 import numpy as np
+from scipy.stats import norm, lognorm, expon, triang, uniform
+import matplotlib.pyplot as plt
 
 #%% Test poes function
 area = 20 #acres
@@ -23,3 +25,30 @@ boi = np.array([1.15, 1.25, 1.30, 1.18, 1.51])
 
 Poes_array = np.round(poes(area, h, poro, swi, boi), 2)
 print(Poes_array)
+
+#%%Generate random values for Thickness - Lissette
+# Generate random values for thickness (h)
+
+# Normal distribution
+h_norm = norm.rvs(loc=30, scale=0.5, size=1000)
+
+# Lognormal distribution
+h_lognorm = lognorm.rvs(s=0.5, loc=0, scale=30, size=1000)
+
+# Exponential distribution
+h_expon = expon.rvs(loc=0, scale=30, size=1000)
+
+# Triangular distribution
+h_tri = triang.rvs(c=0.3, loc=10, scale=80, size=1000)
+
+# Uniform distribution
+h_uni = uniform.rvs(loc=10, scale=80, size=1000)
+
+#plots
+print(h_norm[:10])
+
+plt.hist(h_tri, bins=100, color='skyblue', edgecolor='black')
+plt.title("Distribución Triangular - Espesor (h)")
+plt.xlabel("Espesor (ft)")
+plt.ylabel("Frecuencia")
+plt.show()
